57b69e133d1c5cf6035728225c3553ee9574b5cc
gumingcn <gumingcn@gmail.com>
2023-05-04 16:40:25 +0800

    add webserver

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -10,15 +10,35 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "us-east-2"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-03e312c9b09e29831"
+  ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data = <<-EOF
+                #! /bin/bash
+                echo "hello world" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
 
+  user_data_replace_on_change = true
   tags = {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_security_group" "instance" {
+
+  name = var.security_group_name
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 
